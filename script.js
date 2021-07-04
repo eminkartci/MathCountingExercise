12c123fce4818d94d22ec6f0b714b0d345cfc61b
DurmushKartci <durmusikartci@gmail.com>
2021-07-04 10:38:29 +0300

    Oluşturulan bölme işlemlerindeki hatalar giderildi , daha mantıklı bir oluşturma sistemi eklendi/Javascript

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-
 let questionCount = document.getElementById('quizNumber');
 let resultCount = document.getElementById("results");
-let index = 0,totalQuestionCount = 10;
-let result = 0,trueCount = 0,falseCount = 0;
+let index = 0,
+    totalQuestionCount = 10;
+let result = 0,
+    trueCount = 0,
+    falseCount = 0;
 
 function updateCount() {
 
-    if(index == 10){
+    if (index == 10) {
         return;
     }
 
@@ -16,9 +17,9 @@ function updateCount() {
 
 }
 
-function updateResult(){
+function updateResult() {
 
-    resultCount.innerHTML = trueCount+ ' / ' + (falseCount + trueCount);
+    resultCount.innerHTML = trueCount + ' / ' + (falseCount + trueCount);
 
 }
 
@@ -27,17 +28,17 @@ updateCount();
 updateResult();
 
 
-function getAnswer(){
+function getAnswer() {
 
-    
-    
+
+
 
     let studentAnswer = document.getElementById("studentAnswer").value;
 
-    if(studentAnswer == result){
+    if (studentAnswer == result) {
         document.getElementById("questionCard").style.background = '#edfff0';
         trueCount++;
-    }else{
+    } else {
         document.getElementById("questionCard").style.background = '#fde7e8';
         falseCount++;
     }
@@ -46,40 +47,59 @@ function getAnswer(){
     updateCount();
     updateResult();
     generateQuestion();
-    
+
 }
 
-function generateQuestion(){
+function generateQuestion() {
 
-    let number1 = 0, number2 = 0;
+    let number1 = 0,
+        number2 = 0;
     let operation = '+';
 
     number1 = Math.floor(Math.random() * 100);
     number2 = Math.floor(Math.random() * 100);
 
-    let randomOperator =Math.floor(Math.random() * 4);
+    let randomOperator = Math.floor(Math.random() * 4);
 
-    if(randomOperator == 0){
+    if (randomOperator == 0) {
         operation = '+';
         result = number1 + number2;
-    }else if(randomOperator == 1){
+    } else if (randomOperator == 1) {
         operation = '-';
         result = number1 - number2;
-    }else if(randomOperator == 2){
+    } else if (randomOperator == 2) {
         operation = '/';
+        number2 = generateRandomDisivorOfNumber(number1)
         result = number1 / number2;
-    }else if(randomOperator == 3){
+    } else if (randomOperator == 3) {
         operation = '.';
         result = number1 * number2;
     }
 
 
-    let question = number1 + " " +operation + " "  + number2 + " = x denklemine göre x'in değeri kaçtır ?";
+    let question = number1 + " " + operation + " " + number2 + " = x denklemine göre x'in değeri kaçtır ?";
     let answer = 0;
-    
+
 
 
     document.getElementById("question").innerHTML = question;
 
 }
+
+function generateRandomDisivorOfNumber(sayi) {
+    let array = [];
+    for (let i = 1; i <= sayi; i++) {
+        if (sayi % i == 0) {
+            array.push(i);
+        }
+
+    }
+    array.push(sayi * 2)
+    array.push(sayi * 3)
+    array.push(sayi * 4)
+
+    let random = Math.floor(Math.random() * array.length)
+    return array[random];
+}
+
 generateQuestion();
