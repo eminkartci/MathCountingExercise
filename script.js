6aaf3a2f16ff95869c1984b49836484988bf3e9a
DurmushKartci <durmusikartci@gmail.com>
2021-07-04 18:00:31 +0300

    Tespit edilen hatalar düzeltildi

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-
 let questionCount = document.getElementById('quizNumber');
 let resultCount = document.getElementById("results");
-let index = 0,totalQuestionCount = 10;
-let result = 0,trueCount = 0,falseCount = 0;
+let index = 0,
+    totalQuestionCount = 10;
+let result = 0,
+    trueCount = 0,
+    falseCount = 0;
 
 function updateCount() {
 
-    if(index == 10){
+    if (index == 10) {
         return;
     }
 
@@ -16,37 +17,38 @@ function updateCount() {
 
 }
 
-function updateResult(){
+function updateResult() {
 
-    resultCount.innerHTML = trueCount+ ' / ' + (falseCount + trueCount);
+    resultCount.innerHTML = trueCount + ' / ' + (falseCount + trueCount);
 
 }
 
-function buttonHandler(){
+function buttonHandler() {
 
     let submitBtn = document.getElementById("submit");
 
-    if(submitBtn.innerHTML == "Cevapla"){
+    if (submitBtn.innerHTML == "Cevapla") {
         getAnswer();
-    }else if(submitBtn.innerHTML == "Atla"){
+    } else if (submitBtn.innerHTML == "Atla") {
         revealResult();
-    }else if(submitBtn.innerHTML == "Geç"){
+    } else if (submitBtn.innerHTML == "Geç") {
         generateQuestion();
     }
 
 }
 
-function getAnswer(){
+function getAnswer() {
 
     let studentAnswer = document.getElementById("studentAnswer").value;
 
-    if(studentAnswer == result){
+    if (studentAnswer == result) {
         document.getElementById("questionCard").style.background = '#edfff0';
         trueCount++;
         updateCount();
         updateResult();
         generateQuestion();
-    }else{
+        resetFields();
+    } else {
         document.getElementById("questionCard").style.background = '#fde7e8';
         falseCount++;
 
@@ -54,17 +56,17 @@ function getAnswer(){
     }
 
     //console.log("Öğrenci Cevabı: ",studentAnswer);
-    
-    
+
+
 }
 
-function resetFields(){
+function resetFields() {
     document.getElementById("submit").innerHTML = "Atla";
     document.getElementById("studentAnswer").value = "";
     document.getElementById("studentAnswer").style.backgroundColor = 'rgba(100,200,100,0.1)';
 }
 
-function revealResult(){
+function revealResult() {
 
     let field = document.getElementById("studentAnswer");
 
@@ -77,34 +79,35 @@ function revealResult(){
     document.getElementById("submit").innerHTML = "Geç";
 }
 
-function generateQuestion(){
+function generateQuestion() {
 
-    let number1 = 0, number2 = 0;
+    let number1 = 0,
+        number2 = 0;
     let operation = '+';
 
     number1 = Math.floor(Math.random() * 100);
     number2 = Math.floor(Math.random() * 100);
 
-    let randomOperator =Math.floor(Math.random() * 4);
+    let randomOperator = Math.floor(Math.random() * 4);
 
-    if(randomOperator == 0){
+    if (randomOperator == 0) {
         operation = '+';
         result = number1 + number2;
-    }else if(randomOperator == 1){
+    } else if (randomOperator == 1) {
         operation = '-';
         result = number1 - number2;
-    }else if(randomOperator == 2){
+    } else if (randomOperator == 2) {
         operation = '/';
         result = number1 / number2;
-    }else if(randomOperator == 3){
+    } else if (randomOperator == 3) {
         operation = '.';
         result = number1 * number2;
     }
 
 
-    let question = number1 + " " +operation + " "  + number2 + " = x denklemine göre x'in değeri kaçtır ?";
+    let question = number1 + " " + operation + " " + number2 + " = x denklemine göre x'in değeri kaçtır ?";
     let answer = 0;
-    
+
 
 
     document.getElementById("question").innerHTML = question;
@@ -120,9 +123,9 @@ let inputField = document.getElementById("studentAnswer");
 let submitButton = document.getElementById("submit");
 inputField.onchange = () => {
 
-    if(inputField.value == ""){
+    if (inputField.value == "") {
         submitButton.innerHTML = "Atla";
-    }else{
+    } else {
         submitButton.innerHTML = "Cevapla";
     }
 }
