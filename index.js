2b8fd99d1e918d7fe115bd5486aaad6ab93559ad
eminkartci <emin.kartci@ozu.edu.tr>
2021-07-10 10:18:06 +0300

    server continuing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+
+
+const app = express();
+const router = express.Router();
+const port = 4000
+
+
+app.get('/', (req,res) => {
+    res.sendFile("index.html", {root: path.resolve()})
+})
+
+
+app.listen(port, () => {
+    console.log("Listening ", port)
+})
