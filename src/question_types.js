78944e661741e7fe65a3e845ac79f7d1480e8b35
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-08 10:36:16 +0300

    Yeni soru tipi eklendi

--- src/question_types.js
+++ src/question_types.js
@@ -1,15 +1,17 @@
-let operators = ["+", "-", ".", "/"]
-let question_types = [question_1()]
 
-function question_1() {
+
+let operators = ["+", "-", ".", "/"]
+let question_types = [easyQuestion()]
+
+function easyQuestion() {
 
     let number_1 = 0,
         number_2 = 0;
 
-    number_1 = Math.floor(Math.random() * 100);
-    number_2 = Math.floor(Math.random() * 100);
+    number_1 = randBetween(100);
+    number_2 = randBetween(100);
 
-    let operator = operators[Math.floor(Math.random() * 4)];
+    let operator = operators[randBetween(4)];
     let result = 0;
     if (operator == "+")
         result = number_1 + number_2;
@@ -22,27 +24,27 @@ function question_1() {
         result = number_1 / number_2;
     }
     let question = number_1 + " " + operator + " " + number_2 + " = x denklemine göre x'in değeri kaçtır ?"
-    console.log(question)
-    console.log(number_1 + " " + operator + " " + number_2);
-    console.log(result);
+    // console.log(question)
+    // console.log(number_1 + " " + operator + " " + number_2);
+    // console.log(result);
     return [
         question,
         result
     ]
 }
 
-function question_2() {
+function midQuestion() {
 
     let k_1 = 1,
         k_2 = 1,
         number_1 = 0;
 
-    k_1 = Math.floor(Math.random() * 5)
-    k_2 = k_1 * Math.floor(Math.random() * 5) + 1
-    number_1 = k_1 * Math.floor(Math.random() * 5) + 1
+    k_1 = randBetween(5)
+    k_2 = k_1 * randBetween(5) + 1
+    number_1 = k_1 * randBetween(5) + 1
     let question = "";
 
-    let operator = operators[Math.floor(Math.random() * 4)];
+    let operator = operators[randBetween(4)];
     if (k_1 = 0 && operator != "/" && operator != "x")
         question = number_1 + " = " + k_2 + "x denklemine göre x'in değeri kaçtır ?"
     else
@@ -67,6 +69,36 @@ function question_2() {
     ]
 }
 
-function reload_questions() {
-    question_types = [question_1()]
+
+function constructionProblemQuestion() {
+
+    // 1 - Change the multiplication / division
+    // 2 - Change the workerNum - workerTime 
+    // 3 - Multiplier 
+    //      + common divider if division
+    //      + dosn't matter if multiplication
+    // 4 - Change the answer: newworkernum / newworktime
+
+    var workerNum = randBetween(20),workTime= randBetween(60),multiplier = randBetween(10),newWorkerNum = workerNum * multiplier; newWorkTime = workTime / multiplier;
+
+    var questionStr = `Bir işi ${workerNum} işçi ${workTime} günde bitirebiliyorsa, aynı işi ${newWorkerNum} işçi kaç günde bitirir?`;
+
+    console.log("Question: ", questionStr)
+    console.log("Answer:", newWorkTime)
+
 }
+
+function reload_questions() {
+    question_types = [easyQuestion()]
+}
+
+function randBetween(stop,start=0){
+    // console.log("Start",start)
+    // console.log("stop",stop)
+    return start + Math.floor(Math.random() * (stop-start))
+}
+
+
+
+
+constructionProblemQuestion();
