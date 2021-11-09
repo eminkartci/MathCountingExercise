578baa2c290024b1b1985a4bfa2f45819573770c
DurmushKartci <durmusikartci@gmail.com>
2021-11-09 19:22:28 +0200

    template eklendi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 const app = express();
 const router = express.Router();
-const port = 8800
+const port = 5003
 
 app.use(express.static("src"))
 app.use("/css", express.static(path.resolve() + "src/css"))
@@ -13,7 +13,7 @@ app.use("/js", express.static(path.resolve() + "src/js"))
 app.use("/images", express.static(path.resolve() + "src/images"))
 
 app.get('/', (req, res) => {
-    res.sendFile('./src/index.html', { root: path.resolve() })
+    res.sendFile('./src/views/quiz.html', { root: path.resolve() })
 })
 
 
