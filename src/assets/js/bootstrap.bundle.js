15219c25a2cf19bf7eda2afee99ebdb04a4c1536
DurmushKartci <durmusikartci@gmail.com>
2021-11-13 13:56:53 +0200

    class yapısı başlandı

--- src/assets/js/bootstrap.bundle.js
+++ src/assets/js/bootstrap.bundle.js
@@ -3,7 +3,7 @@
   * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
-(function (global, factory) {
+  (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.bootstrap = {}, global.jQuery));
