509cc0c4b47e04adae2ba14512cbaa36b89d3ab7
DurmushKartci <durmusikartci@gmail.com>
2022-01-01 14:43:49 +0300

    bir soru açıldığında diğer sorular kapatılma özelliği geldi

--- web/src/assets/js/quiz.js
+++ web/src/assets/js/quiz.js
@@ -1,6 +1,8 @@
 
 let sorular_div = document.getElementById("sorular_div")
 let soru_sayisi = 0
+// Hangi soruların görünüp görünmediği bu divde boolen olarak tutulur
+let soru_gorunum_boolen_array = []
 
 // Databaseden alacağımız dersler için dersler değişkenini oluşturuyoruz
 let test;
@@ -23,31 +25,56 @@ myPromise.then(
   function(test_json_value) {
       test = JSON.parse(test_json_value)
       console.log(test)
-
+      let current_soru_sayisi = 0
       for(let i = 0;i<Object.keys(test).length;i++){
-          if(i != 0){
-            soru_ekle(test["Soru"+i])
-          }
+          
+            if(i != 0){
+            current_soru_sayisi += 1;
+            soru_ekle(test["Soru"+i],current_soru_sayisi)
+            }   
       }
+
+      soru_sayisi = Object.keys(test).length
+      soru_gorunum_butonlarini_etkinlestir()
   }
 )
 
-function soru_ekle(soru){
-    soru_sayisi += 1;
+function soru_ekle(soru,current_soru_sayisi){
     let siklar_HTML = ""
+    let buttons_div =""
+    soru_gorunum_boolen_array.push(false)
 
     for(let i = 0;i<Object.keys(soru.Siklar).length;i++){
         siklar_HTML += 
         `
         <div class="custom-control custom-radio">
-            <input type="radio" id="customRadio${i+1}_${soru_sayisi}"
-                name="customRadio_${soru_sayisi}" class="custom-control-input">
+            <input type="radio" id="customRadio${i+1}_${current_soru_sayisi}"
+                name="customRadio_${current_soru_sayisi}" class="custom-control-input">
             <label class="custom-control-label"
-                for="customRadio${i+1}_${soru_sayisi}">${soru.Siklar[i]}</label>
+                for="customRadio${i+1}_${current_soru_sayisi}">${soru.Siklar[i]}</label>
         </div>
         `
     }
-
+    if(current_soru_sayisi == 1){
+        buttons_div = `
+            <div class="row" style="display:flex;justify-content:center">
+            <button class="btn btn-primary" id="next_q_button_${current_soru_sayisi + 1}" style="margin-left:5px;">İleri</button>
+            </div>
+        `
+    }else if(current_soru_sayisi == Object.keys(test).length - 1){
+        buttons_div = `
+            <div class="row" style="display:flex;justify-content:center">
+            <button class="btn btn-primary" id="prev_q_button_${current_soru_sayisi - 1}" style="margin-left:5px;border:none;background:red">Geri</button>
+            </div>
+        `
+    }else{
+        buttons_div = `
+            <div class="row" style="display:flex;justify-content:center">
+            <button class="btn btn-primary" id="prev_q_button_${current_soru_sayisi - 1}" style="margin-left:5px;border:none;background:red">Geri</button>
+            <button class="btn btn-primary" id="next_q_button_${current_soru_sayisi + 1}" style="margin-left:5px;">İleri</button>
+            </div>
+        `
+    }
     sorular_div.innerHTML += 
      `
      <div>
@@ -56,37 +83,33 @@ function soru_ekle(soru){
                     <div
                         class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
                         <div class="list-item-heading mb-0 truncate w-80 mb-1 mt-1">
-                            <span class="heading-number d-inline-block">
-                                ${soru_sayisi}
+                            <span class="heading-number d-inline-block" >
+                                ${current_soru_sayisi}
                             </span>
                             ${soru.Ders.DersYazisi}
                         </div>
                     </div>
                     <div
                         class="custom-control custom-checkbox pl-1 align-self-center pr-4">
-                        <button class="btn btn-outline-theme-3 icon-button edit-button">
-                            <i class="simple-icon-pencil"></i>
-                        </button>
                         <button
                             class="btn btn-outline-theme-3 icon-button rotate-icon-click"
-                            type="button" data-toggle="collapse" data-target="#q${soru_sayisi}"
-                            aria-expanded="false" aria-controls="q${soru_sayisi}">
+                            type="button" data-toggle="collapse" data-target="#q${current_soru_sayisi}"
+                            aria-expanded="false" aria-controls="q${current_soru_sayisi}"
+                            id="question_toggle_button_${current_soru_sayisi}"
+                            >
                             <i class="simple-icon-arrow-down with-rotate-icon"></i>
                         </button>
                     </div>
                 </div>
 
-                <div class="collapse question-collapse" id="q${soru_sayisi}">
+                <div class="collapse question-collapse" id="q${current_soru_sayisi}">
                     <div class="card-body pt-0">
                         
                         <label>${soru.SoruYazisi}</label>
                         <div class="mb-4">
                         ${siklar_HTML}
                         </div>
-                        <div class="row" style="display:flex;justify-content:center">
-                            <button class="btn btn-primary" style="background:red">Geri</button>
-                            <button class="btn btn-primary">İleri</button>
-                        </div>
+                        ${buttons_div}
                     </div>
                     
                 </div>
@@ -95,3 +118,18 @@ function soru_ekle(soru){
      `
 }
 
+function soru_gorunum_butonlarini_etkinlestir(){
+    for(let current_soru_sayisi = 1;current_soru_sayisi < soru_sayisi;current_soru_sayisi ++){
+        console.log("question_toggle_button_"+current_soru_sayisi)
+        document.getElementById("question_toggle_button_"+current_soru_sayisi).onclick = () => {
+            for(let i = 0;i< soru_gorunum_boolen_array.length;i++){
+                if(soru_gorunum_boolen_array[i] == true){
+                    document.getElementById("question_toggle_button_"+(i+1)).click()
+                    soru_gorunum_boolen_array[i] = false
+                }
+            }
+            soru_gorunum_boolen_array[current_soru_sayisi-1]= true
+            console.log("hello",current_soru_sayisi)
+        }
+    }
+}
